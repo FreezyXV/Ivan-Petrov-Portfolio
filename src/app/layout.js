95e96ae0710f076c213188cdb93940328ca49b51
FreezyXV <i_841@mail.ru>
2025-01-18 13:18:03 +0100

    Some Images formats and locations updates

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,28 +1,27 @@
-import './globals.scss'
-import { Inter } from 'next/font/google'
+// app/layout.js
+import "./globals.scss";
+import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Ivan Petrov",
+  title: "Ivan Petrov Portfolio",
   description: "Personal portfolio",
+  icons: {
+    icon: [
+      { url: "/favicon-16x16.png", sizes: "16x16" },
+      { url: "/favicon-32x32.png", sizes: "32x32" },
+      { url: "/favicon.ico", sizes: "any" },
+    ],
+    apple: [{ url: "/apple-touch-icon.png", sizes: "180x180" }],
+  },
+  manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={inter.className}>
-      <head>
-      <link rel="icon" href="/Ivan-Petrov-Portfolio/src/app/favicon.ico" />
-
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="manifest" href="/site.webmanifest" />
-        <title>Ivan Petrov Portfolio</title>
-        <meta name="description" content="Personal portfolio of Ivan Petrov." />
-      </head>
       <body style={{ margin: 0, padding: 0 }}>{children}</body>
     </html>
   );
 }
-
