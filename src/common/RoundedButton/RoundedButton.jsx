b047c2fe98fb639d424ea54fa553086484399bc6
FreezyXV <i_841@mail.ru>
2025-01-18 14:11:48 +0100

    Rounding Button update

--- src/common/RoundedButton/RoundedButton.jsx
+++ src/common/RoundedButton/RoundedButton.jsx
@@ -1,58 +1,40 @@
-import React, { useEffect, useRef } from "react";
-import gsap from "gsap";
-import Magnetic from "../Magnetic/Magnetic";
-import styles from "./style.module.scss";
+import React from 'react'
+import { useEffect, useRef } from 'react';
+import styles from './style.module.scss';
+import gsap from 'gsap';
+import Magnetic from '../Magnetic/Magnetic';
 
-export default function RoundedButton({
-  children,
-  backgroundColor = "#455CE9",
-  ...attributes
-}) {
+export default function RoundedButton({children, backgroundColor="#455CE9", ...attributes}) {
+
   const circle = useRef(null);
-  const timeline = useRef(null);
-  const timeoutId = useRef(null);
-
-  useEffect(() => {
-    if (!circle.current) return;
-
-    timeline.current = gsap.timeline({ paused: true });
+  let timeline = useRef(null);
+  let timeoutId = null;
+  useEffect( () => {
+    timeline.current = gsap.timeline({paused: true})
     timeline.current
-      .to(circle.current, {
-        top: "-25%",
-        width: "150%",
-        duration: 0.4,
-        ease: "power3.in",
-      })
-      .to(circle.current, { top: "-150%", width: "125%", duration: 0.25 });
-  }, []);
-
+      .to(circle.current, {top: "-25%", width: "150%", duration: 0.4, ease: "power3.in"}, "enter")
+      .to(circle.current, {top: "-150%", width: "125%", duration: 0.25}, "exit")
+  }, [])
+  
   const manageMouseEnter = () => {
-    if (timeoutId.current) clearTimeout(timeoutId.current);
-    timeline.current.tweenFromTo("enter", "exit");
-  };
+    if(timeoutId) clearTimeout(timeoutId)
+    timeline.current.tweenFromTo('enter', 'exit');
+  }
 
   const manageMouseLeave = () => {
-    timeoutId.current = setTimeout(() => {
+    timeoutId = setTimeout( () => {
       timeline.current.play();
-    }, 300);
-  };
+    }, 300)
+  }
 
   return (
     <Magnetic>
-      <div
-        className={styles.roundedButton}
-        style={{ overflow: "hidden" }}
-        onMouseEnter={manageMouseEnter}
-        onMouseLeave={manageMouseLeave}
-        {...attributes}
-      >
-        {children}
-        <div
-          ref={circle}
-          style={{ backgroundColor }}
-          className={styles.circle}
-        ></div>
+      <div className={styles.roundedButton} style={{overflow: "hidden"}} onMouseEnter={() => {manageMouseEnter()}} onMouseLeave={() => {manageMouseLeave()}} {...attributes}>
+          {
+            children
+          }
+        <div ref={circle} style={{backgroundColor}} className={styles.circle}></div>
       </div>
     </Magnetic>
-  );
+  )
 }
